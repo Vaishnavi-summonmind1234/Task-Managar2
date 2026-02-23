6b3e0975306a6b6c32dbb9e55eefef12e201ae26
Vaishnavi-summonmind1234 <vaishnavi@summonmind.com>
2026-02-23 15:49:48 +0530

    updated

--- frontend/taskmanagement/src/services/auth_services.ts
+++ frontend/taskmanagement/src/services/auth_services.ts
@@ -1,28 +1,35 @@
 import axiosinstance from "./api";
 
-const register =async(data:{
-    name:string,
-    email:string,
-    password:string,
-    role_id:number})=>{
-           const rs=await axiosinstance.post('/auth/register')
-           return rs.data
 
-    };
-const login =async(data:{
-    email:string,
-    password:string})=>{
-        const res=await axiosinstance.post('/auth/login')
-        localStorage.setItemt('access_token',res.data.access_token)
-        return res.data
-
-    };
-const get_profile=async()=>{
-    const res=await axiosinstance.get('/auth/me/')
-    return res.data
+export const register = async (data: {
+  name: string;
+  email: string;
+  password: string;
+  role_id: number;
+}) => {
+  const res = await axiosinstance.post("/auth/register", data);
+  return res.data;
 };
 
-const logout=()=>{
-    const res=localStorage.removeItem('access_token')
-    
-}
+
+export const login = async (data: {
+  email: string;
+  password: string;
+}) => {
+  const res = await axiosinstance.post("/auth/login", data);
+
+ 
+  localStorage.setItem("access_token", res.data.access_token);
+
+  return res.data;
+};
+
+export const getProfile = async () => {
+  const res = await axiosinstance.get("/auth/me");
+  return res.data;
+};
+
+// LOGOUT
+export const logout = () => {
+  localStorage.removeItem("access_token");
+};
---
+++ frontend/taskmanagement/src/services/user_detail_services.ts
@@ -0,0 +1,41 @@
+import axiosinstance from "./api";
+
+
+export const userDetails = async () => {
+  const res = await axiosinstance.get("/user/user-details/");
+  return res.data;
+};
+
+
+export const userById = async (id: number) => {
+  const res = await axiosinstance.get(`/user/userby/${id}`);
+  return res.data;
+};
+
+
+export const addUser = async (data: {
+  name: string;
+  email: string;
+  password: string;
+  role_id: number;
+}) => {
+  const res = await axiosinstance.post("/user/add-user", data);
+  return res.data;
+};
+
+
+export const updateUser = async (
+  id: number,
+  data: {
+    name: string;
+    email: string;
+  }
+) => {
+  const res = await axiosinstance.put(`/user/update/${id}`, data);
+  return res.data;
+};
+
+export const deleteUser = async (id: number) => {
+  const res = await axiosinstance.delete(`/user/delete-use/${id}`);
+  return res.data;
+};
